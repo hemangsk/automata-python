01b94a29947c2394fbe6a3bdead5853737f168a5
Hemang Kumar <hemangsk@gmail.com>
2016-10-21 01:07:51 +0530

    update language.py

--- automata/fa/DFA.py
+++ automata/fa/DFA.py
@@ -2,15 +2,20 @@
 class DFA(object):
     """Deterministic Finite Automata"""
 
-    def __init__(self, Q, sigma, delta, q0, F):
+    def __init__(self, states, alphabet, transitions, initial_state, final_states):
         """
         Q is a finite set of states.
 
         sigma is a finite set of symbols called the alphabet.
 
-        delta is the transition function where delta: Q x sigma → Q
+        delta is the list of transitions based on transition relation.
 
         q0 is the initial state from where any input is processed (q0 ∈ Q).
 
         F is a set of final state/states of Q (F ⊆ Q).
         """
+        self.states = states
+        self.alphabet = alphabet
+        self.transitions = transitions
+        self.initial_state = initial_state
+        self.final_states = final_states
--- automata/language.py
+++ automata/language.py
@@ -1,5 +1,46 @@
-def length(string):
-    return len(str(string))
+class Language(object):
+    """Basic operations that can be performed on a language"""
 
-def reverse(string):
-    return string[::1]
+    def __init__(self, language, alphabets, rules):
+        """Constructor to create language and its alphabet set"""
+        if(type(language) == "list"):
+            self.language = language
+
+            if(type(alphabets) == "list"):
+                self.alphabets = alphabets
+            else:
+                self.alphabets = []
+
+            if(type(rules) == "list"):
+                self.rules = rules
+            else:
+                self.rules = []
+
+    def define(self, strings):
+        """Add words to the language"""
+
+    def define_alphabets(self, element):
+        """"Add elements to the set of alphabets of the language"""
+
+        if(type(element) == "list"):
+                self.alphabet = element
+
+        else:
+            self.alphabet.push(element)
+
+    def define_rules(self, rule ):
+        """Add rule string to python list of rules"""
+        self.rule.push(rule)
+
+    def show(self):
+        """Getter for language"""
+        output = "----------------\n"
+        output += "Language is " + self.language + "\n\n"
+        output += "Alphabet for it is " + self.alphabets + "\n\n"
+        output += "Rules for this language are " + self.rules + "\n"
+        output += "\n----------------"
+        return output
+
+    def show_alphabet(self):
+        """Getter for alphabets"""
+        return self.alphabets
